efcf68f0a7cb48e335b38365199481deeb9bdd32
Sourav Majumdar <105838250+souravmusquaretech@users.noreply.github.com>
2022-06-04 13:18:55 +0530

    Adding yml config files

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.get('/api/user/all',(req,res,next)=>{
+  res.send('Hello world!!!')
+})
+
 app.listen(4000, () => console.log('Listen ton port 4000'));
